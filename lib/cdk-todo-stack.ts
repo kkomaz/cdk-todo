963d9245b30dc15f35dd54db4582945396e91ef1
kkomaz <Alex Lee>
2021-05-10 22:47:18 -0400

    Basic lambda function

--- lib/cdk-todo-stack.ts
+++ lib/cdk-todo-stack.ts
@@ -1,18 +1,23 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
-
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apiGateway from '@aws-cdk/aws-apigateway';
 export class CdkTodoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkTodoQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const helloLambda = new lambda.Function(this, 'hello', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lib/lambda'),
+      handler: 'handler',
+      memorySize: 256,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        isProduction: 'absolutely not',
+      },
     });
 
-    const topic = new sns.Topic(this, 'CdkTodoTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new apiGateway.LambdaRestApi(this, 'todo-endpoint', {
+      handler: helloLambda,
+    });
   }
 }
