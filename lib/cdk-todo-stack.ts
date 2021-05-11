cead0520615b70aa7435e4aabed0255cf0e8b45d
kkomaz <Alex Lee>
2021-05-11 00:00:10 -0400

    add and get

--- lib/cdk-todo-stack.ts
+++ lib/cdk-todo-stack.ts
@@ -1,23 +1,26 @@
 import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
 import * as apiGateway from '@aws-cdk/aws-apigateway';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+
+import { TodoBackend } from './todo-backend';
 export class CdkTodoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new lambda.Function(this, 'hello', {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.fromAsset('lib/lambda'),
-      handler: 'handler',
-      memorySize: 256,
-      timeout: cdk.Duration.seconds(10),
-      environment: {
-        isProduction: 'absolutely not',
-      },
+    const todoBackend = new TodoBackend(this, 'TodoBackend');
+
+    new apiGateway.LambdaRestApi(this, 'Endpoint', {
+      handler: todoBackend.handler,
     });
 
-    new apiGateway.LambdaRestApi(this, 'todo-endpoint', {
-      handler: helloLambda,
+    const logoBucket = new s3.Bucket(this, 'LogoBucket', {
+      publicReadAccess: true,
+    });
+
+    new s3Deployment.BucketDeployment(this, 'DeployLog', {
+      destinationBucket: logoBucket,
+      sources: [s3Deployment.Source.asset('./assets')],
     });
   }
 }
